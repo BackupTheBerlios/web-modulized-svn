888a37b8b663ba3a54c50bb3ac5e3530dc29b165
xmajipoor <xmajipoor@bf19e62a-7d05-0410-8961-c54779380810>
2005-11-14 21:55:36 +0000

    eclipse error

--- trunk/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Created on 06.11.2005
- *
- * This is the entry file for the frame work. index.html and
- * index.htm will only redirect to this file. They are just a
- * part of the simpliest security part.
- */
-?>
