c2cf9901dcbea8d114e32c4ed3f5401762f4b4de
xmajipoor <xmajipoor@bf19e62a-7d05-0410-8961-c54779380810>
2005-11-15 19:41:08 +0000

    added creation timestamp

--- web-modulized/trunk/coremodules/core_module_usermanagement.php
+++ web-modulized/trunk/coremodules/core_module_usermanagement.php
@@ -19,5 +19,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+ /*
+ * Created on 06.11.2005
+ */
  
 ?>
