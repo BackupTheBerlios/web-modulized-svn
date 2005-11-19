b92bc8841613eaff853799f9af14fbaa78b52fd6
xmajipoor <xmajipoor@bf19e62a-7d05-0410-8961-c54779380810>
2005-11-19 00:15:32 +0000

    added exemplary variables for seting up the installation.

---
+++ web-modulized/trunk/conf/MyConf.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Steffen Lorch & Fabian Schorcht                 *
+ *   steffen@web-modulized.org                                             *
+ *   fabian@web-modulized.org                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+ 
+/***************************************************************************
+ *  this file includes the main configuration parameters concerning this 
+ * spezific installtion of WEB-MODULIZED.ORG.
+ * you will have to modify this parameters ether by your favourite editor
+ * or in the administration menu of your side
+***************************************************************************/
+
+/* installation configuration */
+$WEB_MODULIZED_ROOTPATH="";
+
+/* sidespezific configuration */
+$WEB_MODULIZED_DOMAINNAME="";
+$WEB_MODULIZED_SIDENAME="";
+$WEB_MODULIZED_TOPIC="";
+
+/* mysql configuration */
+$MYSQL_SERVER_NAME="";
+$MYSQL_SERVER_USER="";
+$MYSQL_SERVER_DBNAME="";
+$WEB_MODULIZED_DB_PREFIX="";
+
+
+?>
